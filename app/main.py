72f4349cd66fd899ea9069d8559d08c7a668aed8
xbexbex <xbexbex@users.noreply.github.com>
2024-12-19 18:42:51 +0000

    add import-shapefile

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
 from contextlib import asynccontextmanager
+import shutil
+import tempfile
+from pathlib import Path
+from fastapi import UploadFile, File, Form, HTTPException
+from typing import List
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app import config
 from app.utils.logger import get_logger
+from app.db import connection
+from app.utils.geometry import import_shapefile_to_layer
 
 logger = get_logger(__name__)
 global_settings = config.get_settings()
@@ -33,6 +40,42 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-@app.get("/hello")
+
+@app.get(path="/hello")
 async def calculate():
     return {"hi": "there"}
+
+
+@app.post(path="/import-shapefile")
+async def import_shapefile(
+    name: str = Form(...),
+    description: str = Form(None),
+    zip_file: UploadFile = File(...),
+):
+    # Create temporary directory
+
+    if not zip_file.filename:
+        raise HTTPException(
+            status_code=400, detail=f"Missing filename in main .shp file"
+        )
+
+    try:
+        with tempfile.NamedTemporaryFile(suffix=".zip") as temp_file:
+            shutil.copyfileobj(zip_file.file, temp_file)
+            temp_file.flush()
+
+            async with connection.get_async_context_db() as session:
+                layer = await import_shapefile_to_layer(
+                    session, temp_file.name, name, description
+                )
+
+                return {
+                    "id": str(layer.id),
+                    "name": layer.name,
+                    "description": layer.description,
+                }
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=400, detail=f"Failed to import shapefile: {str(e)}"
+        )
