fe279c4f5897bd6ef1df6dc2cc9e10ac59bf8136
xbexbex <22522159+xbexbex@users.noreply.github.com>
2025-07-27 14:44:10 +0000

    Fix deprecated config

--- app/types/general.py
+++ app/types/general.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 from typing import Literal
 
 used_db_types = []
@@ -7,6 +7,8 @@ IndexingStrategy = Literal["name_municipality", "id"]
 
 
 class ColOptions(BaseModel):
+    model_config = ConfigDict(populate_by_name=True)
+    
     indexing_strategy: IndexingStrategy = Field(alias="indexingStrategy")
     id_col: str | None = Field(None, alias="idCol")
     name_col: str = Field(alias="nameCol")
@@ -14,6 +16,3 @@ class ColOptions(BaseModel):
     region_col: str | None = Field(None, alias="regionCol")
     description_col: str | None = Field(None, alias="descriptionCol")
     area_col: str | None = Field(None, alias="areaCol")
-
-    class Config:
-        populate_by_name = True
