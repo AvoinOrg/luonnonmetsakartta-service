65297da680b8bc65543a6a8151c9eccc86567578
xbexbex <xbexbex@users.noreply.github.com>
2025-08-07 13:48:50 +0000

    Add storage vars to config

--- app/config.py
+++ app/config.py
@@ -67,6 +67,9 @@ class Settings(BaseSettings):
     zitadel_project_id: str = os.getenv("ZITADEL_PROJECT_ID") or ""
     zitadel_project_editor_role: str = os.getenv("ZITADEL_PROJECT_EDITOR_ROLE") or ""
     # zitadel_project_admin_role: str = os.getenv("ZITADEL_PROJECT_ADMIN_ROLE") or ""
+    storage_bucket: str = env_vars["STORAGE_BUCKET"]
+    storage_url: str = env_vars["STORAGE_URL"]
+    storage_auth_key: str = env_vars["STORAGE_AUTH_KEY"]
 
 
 @lru_cache
